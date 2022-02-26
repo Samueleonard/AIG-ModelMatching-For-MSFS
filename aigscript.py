d4ad722d7f05558412dd4600040b8193df1b31f4
Samuel Leonard <34142670+Samueleonard@users.noreply.github.com>
2022-02-26 11:40:28 +0000

    LATEST AS OF 26/02/22
    added cl350
    updated filghtplans

--- aigscript.py
+++ aigscript.py
@@ -1,25 +1,60 @@
 from genericpath import exists
+from operator import truediv
 import os
 import shutil
+import time
 
-comFolderPath = "E:\\MSFS\\Community\\Traffic\\aig-aitraffic-oci-beta"
-githubFolderPath = "E:\\AIG-ModelMatching-For-MSFS\\aig-aitraffic-oci-beta"
+comFolderPath = "E:\\MSFS\\Community\\Traffic\\aig-aitraffic-oci-beta\\"
+githubFolderPath = "E:\\AIG-ModelMatching-For-MSFS\\aig-aitraffic-oci-beta\\"
 
-def remove(name):
-    if exists(name):
-        os.remove(name)
+names = ["Effects", "Sound", "Texture", "Traffic Files", "layout.json", "manifest.json", "SimObjects"]
+
+#######################################
+
+
+def clearGithubDir():
+    print("clearing github folder")
+    if exists(githubFolderPath):
+        shutil.rmtree(githubFolderPath)
+    print("github folder cleared")
+    if not os.path.exists(githubFolderPath):
+        os.makedirs(githubFolderPath)
+    
+def copyFiles(toGithub):
+    if(not toGithub):
+        for name in names:
+            print("copying " + githubFolderPath + name + " to " + comFolderPath)
+            shutil.move(githubFolderPath + name, comFolderPath)
+            print("copied " + githubFolderPath + name + " to " + comFolderPath)
+        shutil.move(githubFolderPath + "BritishAvgeeks-AIG-MSFS-Vatsim-Rules.vmr", "C:\\Users\\samue\\Documents\\vPilot Files\\BritishAvgeeks-AIG-MSFS-Vatsim-Rules.vmr")
     else:
-        print(name + " does not exist")
+        for name in names:
+            print("copying " + comFolderPath + name + " to " + githubFolderPath)
+            shutil.move(comFolderPath + name, githubFolderPath)
+            print("copied " + comFolderPath + name + " to " + githubFolderPath)
+        shutil.move("C:\\Users\\samue\\Documents\\vPilot Files\\BritishAvgeeks-AIG-MSFS-Vatsim-Rules.vmr", githubFolderPath)
 
-def move(src, dest, name):
-    if exists(name):
-        shutil.copy(name + "\\path")
-    else:
-        print(name + " does not exist")
+def openGithub():
+    os.startfile("C:\\Users\\samue\\AppData\\Local\\GitHubDesktop\\GitHubDesktop.exe")
+    wait = input("commit your changes and press enter to continue")
 
-remove(githubFolderPath + "\manifest.json")
-move(comFolderPath + "\manifest.json", githubFolderPath + "\manifest.json")
+def createZip():
+    print("creating zip folder at: " + githubFolderPath + ".zip")
+    if(not exists(githubFolderPath + "\\aig-aitraffic-oci-beta.zip")):
+        shutil.make_archive(githubFolderPath, 'zip', "E:\\AIG-ModelMatching-For-MSFS\\aig-aitraffic-oci-beta")
+    print("zip folder created")
 
+def start():
+    clearGithubDir()
+    copyFiles(True)
+    openGithub()
+    createZip()
+    copyFiles(False)
+
+
+#######################################
+
+start()
 
 #delete the old files
 #move new files and folder into dir
