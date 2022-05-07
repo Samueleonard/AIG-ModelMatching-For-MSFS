3cc590aef332982c0b87ccc30532f3c5d0934ec5
Samuel Leonard <34142670+Samueleonard@users.noreply.github.com>
2022-05-07 10:10:54 +0100

    LATEST AS OF 07/05/22

--- aigscript.py
+++ aigscript.py
@@ -7,7 +7,7 @@ import webbrowser
 comFolderPath = "E:\\MSFS\\Community\\Traffic\\aig-aitraffic-oci-beta\\"
 githubFolderPath = "E:\\AIG-ModelMatching-For-MSFS\\aig-aitraffic-oci-beta\\"
 
-names = ["Sound", "Texture", "Traffic Files", "layout.json", "manifest.json", "SimObjects", 'Effects', 'BritishAvgeeks-AIG-MSFS-Vatsim-Rules.vmr']
+names = ["Sound", "Texture", "Traffic Files", "layout.json", "manifest.json", "Effects", 'SimObjects', 'BritishAvgeeks-AIG-MSFS-Vatsim-Rules.vmr']
 
 #######################################
 
@@ -20,7 +20,6 @@ def clearGithubDir(): #clears the github folder of all files and remakes it empt
     print("github folder cleared")
         
 def copyFiles(toGithub):
-
     if(not toGithub): #we are moving files back to the community folder after zipping
         for name in names:
             print("copying " + githubFolderPath + name + " to " + comFolderPath)
@@ -39,8 +38,8 @@ def copyFiles(toGithub):
         if(choice == "y"):
             print("creating zip folder at: " + githubFolderPath + "aig-aitraffic-oci-beta.zip")
             if(not exists(githubFolderPath + "\\aig-aitraffic-oci-beta.zip")):
-                shutil.make_archive(githubFolderPath, 'zip', "E:\\AIG-ModelMatching-For-MSFS\\aig-aitraffic-oci-beta")
-            print("zip folder created")
+                shutil.make_archive(githubFolderPath, 'zip', githubFolderPath+"\\aig-aitraffic-oci-beta")
+            print("zip folder created at: " + githubFolderPath + "aig-aitraffic-oci-beta.zip")
             webbrowser.open("https://drive.google.com/drive/u/1/my-drive")
             webbrowser.open("https://github.com/Samueleonard/AIG-ModelMatching-For-MSFS/releases/new")
             wait = input("commit your changes and press enter to continue")
