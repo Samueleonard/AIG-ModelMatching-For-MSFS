deb24c9c9323ea207ea9061cda6b11b3383eeb36
Samuel Leonard <34142670+Samueleonard@users.noreply.github.com>
2022-08-10 20:04:13 +0100

    LATEST AS OF 10/08/22

--- aigscript.py
+++ aigscript.py
@@ -14,12 +14,13 @@ names = ["Sound", "Texture", "Traffic Files", "layout.json", "manifest.json", "E
 #######################################
 
 ##AIG is adding payware models - obviously these cannot be distributed so we must remove the individual payware models from the folder and back into the main aig folder
-paywareModelNames = ["aig 1", "aig 2"]
 def handlePaywareModels(): #move out payware files before commiting/shipping
+    paywareModelNames = ["aig 1", "aig 2"]
     for i in range(len(paywareModelNames)):
         if exists(githubFolderPath + "\\simobjects\\airplanes\\" + paywareModelNames[i]):
             print("moving: " + githubFolderPath + "\\simobjects\\airplanes\\" + paywareModelNames[i] + " to " + comFolderPath + "\\simobjects\\airplanes\\" + paywareModelNames[i])
             #shutil.move(githubFolderPath ) 
+            print("moved: " + githubFolderPath + "\\simobjects\\airplanes\\" + paywareModelNames[i] + " to " + comFolderPath + "\\simobjects\\airplanes\\" + paywareModelNames[i])
         else:
             print(githubFolderPath + "\\simobjects\\airplanes\\" + paywareModelNames[i] + " does not exist")
 
@@ -40,12 +41,18 @@ def copyFiles(toGithub):
             print("copied " + githubFolderPath + name + " to " + comFolderPath)
         shutil.rmtree(githubFolderPath) #clear github folder after moving everything back to community
     else: #we are moving files into the github folder for zipping
+        handlePaywareModels()
         for name in names:
             print("copying " + comFolderPath + name + " to " + githubFolderPath)
             shutil.move(comFolderPath + name, githubFolderPath)
             print("copied " + comFolderPath + name + " to " + githubFolderPath)
+
+        #very weird random bug where the sound folder would not be fully moved over - only the AI folder inside - fix that
+        print((githubFolderPath + "Sound\AI"))
+        if not exists(githubFolderPath + "Sound\AI"): #if this folder doesnt exist then the sound folder was not copied properly
+            os.mkdir(githubFolderPath + "Sound\\AI")
         
-        os.startfile("C:\\Users\\samue\\AppData\\Local\\GitHubDesktop\\GitHubDesktop.exe")
+        os.startfile(os.getenv('LOCALAPPDATA') + "\\GitHubDesktop\\GitHubDesktop.exe")
     
         choice = input("Do you want to create a zip file? (y/n)").lower()
         if(choice == "y"):
